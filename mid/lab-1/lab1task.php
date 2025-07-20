5adaf423a5cd73d227409088c6126e34b784d378
MD.MRIDUL <mridulaiub17@gmail.com>
2025-07-20 16:00:46 +0600

    added complete registration form

--- mid/lab-1/lab1task.php
+++ mid/lab-1/lab1task.php
@@ -6,6 +6,53 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <center>
+        <p style="color:blue"><b>AIUB</b></p>
+        <p style="color:blue"><b>Registration Form</b></p>
+    </center>
+    <p><b>Complete the Registration</b></p>
+    <table>
+        <tr>
+            <td>Full Name:</td><td><input type="text"></td>
+        </tr>
+        <tr>
+            <td>Email:</td><td><input type="email"></td>
+        </tr>
+        <tr>
+            <td>password:</td><td><input type="password"></td>
+        </tr>
+        
+        <tr>
+            <td>Gender:</td><td><input type="radio" name="des"> Male
+            <input type="radio" name="des">Female
+            <input type="radio" name="des">Other
+        </td>
+        </tr>
+        <tr>
+            <td>Language Known:</td><td><input type="checkbox">English
+            <input type="checkbox">Bangla
+            <input type="checkbox">Hindi
+            </td>
+        </tr>
+        <tr>
+            <td>Country Name:</td><td><select name="" id="">
+                <option value="">--select--</option>
+               <option value="">Bangladesh</option>
+               <option value="">Japan</option>
+               <option value="">Canada</option>
+            </select></td>
+        </tr>
+        <tr>
+            <td>Date of Birth:</td><td><input type="date"></td>
+        </tr>
+        <tr>
+            <td>Upload Photo:</td><td><input type="file"></td>
+        </tr>
+        <tr>
+            <td>Comment:</td><td><textarea name="" id="" cols="30" rows="5"></textarea></td>
+        </tr>
+        <td></td>
+       <td><button>Register</button></td>
+    </table>
 </body>
 </html>
