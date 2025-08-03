9d5f46793ee2e23e76177dc3e07901908d8c0eee
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-03 15:50:25 +0600

    added custome style

---
+++ view/mid_la_task3/js/task3_valid.js
@@ -0,0 +1,22 @@
+function handleSubmit()
+{
+    var fName=document.getElementById("fname").value;
+    var lName=document.getElementById("lname").value;
+    var address=document.getElementById("address").value;
+    var city=document.getElementById("city").value;
+    var select=document.getElementById("select").value;
+    var phone=document.getElementById("phn").value;
+    var email=document.getElementById("email").value;
+    var createPass=document.getElementById("crepass").value;
+    var confirmPass=document.getElementById("conpass").value;
+    var donationName=document.getElementById("dname").value;
+    var amount=document.getElementById("amount").value;
+    var checkbox1=document.getElementById("checkbox1").value;
+    var checkbox2=document.getElementById("checkbox2").value;
+    var checkbox3=document.getElementById("checkbox3").value;
+    var checkbox4=document.getElementById("checkbox4").value;
+    var textarea=document.getElementById("textarea").value;
+    var button=document.getElementById("button").value;
+
+    
+}
